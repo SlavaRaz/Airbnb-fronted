e03f37f9ca672c220393a1a351e9edf80b74e640
SlavaRaz <slavarazaranov87@gmail.com>
2025-01-12 16:21:14 +0200

    Add navigation menu

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -2,9 +2,11 @@ import { Link, NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-import { logout } from '../store/actions/user.actions'
 import logo from '../assets/img/various/airbnb.png'
+import { logout } from '../store/actions/user.actions.js'
+
 import { SearchBar } from './SearchBar.jsx'
+import { NavMenu } from './NavMenu.jsx'
 
 export function AppHeader() {
 	const user = useSelector(storeState => storeState.userModule.user)
@@ -28,28 +30,10 @@ export function AppHeader() {
 				</NavLink>
 				<h2 className="logo-text">airbnb</h2>
 			</div>
-				{/* <NavLink to="stay">Stays</NavLink> */}
-			{/* <NavLink to="about">About</NavLink>
-				<NavLink to="chat">Chat</NavLink>
-				<NavLink to="review">Review</NavLink> */}
 			<div className='app-header search-bar'>
 				<SearchBar />
 			</div>
-			{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
-
-			{!user && <NavLink to="login" className="login-link">Login</NavLink>}
-			{
-				user && (
-					<div className="user-info">
-						<Link to={`user/${user._id}`}>
-							{/* {user.imgUrl && <img src={user.imgUrl} />} */}
-							{user.fullname}
-						</Link>
-						{/* <span className="score">{user.score?.toLocaleString()}</span> */}
-						<button onClick={onLogout}>logout</button>
-					</div>
-				)
-			}
+				<NavMenu />			
 		</div >
 	)
 }

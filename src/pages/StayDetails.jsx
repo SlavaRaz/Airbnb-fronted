58ac1ce46d1b4e30b2860298a257cf280cceba4d
odeyasl@gmail.com <YOUR_EMAIL_ADDRESS@EXAMPLE.COM>
2025-01-19 13:53:10 +0200

    add share and save icons

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -1,11 +1,11 @@
-import {StayHeader} from '../cmps/StayHeader'
+import {StayGallery} from '../cmps/StayGallery'
 
 export function StayDetails() {
  
 
   return (
     <section className='stay-details'>
-       <StayHeader/>
+       <StayGallery/>
     </section>
   )
 }
